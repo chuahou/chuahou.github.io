e4709fa2d613405566d562f5d9ab204bac08dc4e
Chua Hou <human+github@chuahou.dev>
2020-05-17 23:27:52 +0800

    quicklinks: add quicklinks

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
+import yaml
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ def index():
             split = link.split()
             dict_links.append((split[0], split[1]))
     return render_template("main.html", center_content=True, links=dict_links)
+
+@app.route("/quicklinks/index.html")
+def quicklinks():
+    categories = []
+    with open("data/quicklinks.yml") as yaml_file:
+        categories = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    return render_template("quicklinks.html", center_content=True,
+            categories=categories)
 
 if __name__ == "__main__":
     freezer = Freezer(app)
