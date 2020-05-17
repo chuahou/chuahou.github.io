e5670a85a50733a37225f8abb163e983bd808d0f
Chua Hou <human+github@chuahou.dev>
2020-05-17 21:00:38 +0800

    flask: add Frozen-Flask and build method
    - Makefile contains commands to do everything automatically
    - all: sets up venv and requirements
    - build: builds static site
    - install: installs static site from flask/ to .
    - uninstall: removes static site from ., flask/precious.txt describes
      what not to remove
    - clean: clean build
    - run: run static site from .
    - requirements: install pip requirements

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("main.html", center_content=True)
+
+if __name__ == "__main__":
+    freezer = Freezer(app)
+    freezer.freeze()
