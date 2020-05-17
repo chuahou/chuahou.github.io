d31fa57d6934c02d0b74396f32cc83fd7da61e0d
Chua Hou <human+github@chuahou.dev>
2020-05-17 21:35:45 +0800

    /: dynamically generate link list

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("main.html", center_content=True)
+    dict_links = []
+    with open("data/main_links.txt") as links_file:
+        links = links_file.read().splitlines()
+        for link in links:
+            split = link.split()
+            dict_links.append((split[0], split[1]))
+    return render_template("main.html", center_content=True, links=dict_links)
 
 if __name__ == "__main__":
     freezer = Freezer(app)
